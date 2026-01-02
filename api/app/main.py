ac852bafdd54bdf831e86e60a5632bb11de1af85
Joao Rodrigues <joao.rodrigues@hey.com>
2026-01-02 15:47:20 +0000

    feat: Simplify import statements in main.py and epcis.py for improved clarity

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from api.app.routes.epcis import compliance
-from api.app.routes import trace
+from app.routes import trace
 from app.routes.epcis import epcis
 from app.routes.gdst import gdst
-from api.app.routes.epcis import events
 from app.core.auth import create_access_token
 from datetime import timedelta
 
--- api/app/routes/epcis/epcis.py
+++ api/app/routes/epcis/epcis.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from app.routes.epcis import events
 from app.routes.epcis import compliance
-from api.app.routes import trace
 
 router = APIRouter(prefix="/epcis", tags=["EPCIS"])
 
